39bef7bd93e8b27d1d56a0d5991f6417780795f2
PawanAK <pawanajjark@gmail.com>
2024-03-31 07:35:30 +0530

    reolved

--- display/src/App.jsx
+++ display/src/App.jsx
@@ -173,11 +173,6 @@ function App() {
         value={userAddress}
         onChange={(e) => setUserAddress(e.target.value)}
       />
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-        onClick={addUser}>
-        Add User
-      </button>
 
       {response && <p className="mt-4">{response}</p>}
     </div>
